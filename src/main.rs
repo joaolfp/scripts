845f44ba9f493468383b789eeaadfcf7c4cc9305
joaolfp <joaolucasfp2001@gmail.com>
2026-02-02 23:38:08 -0300

    feat: add an example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use duct::cmd;
+use anyhow::Result;
+
+fn main() -> Result<()> {
+    // TODO: Tomorrow I need to add all commands I'm going to use
+    cmd!("git", "status").run()?;
+    Ok(())
 }
