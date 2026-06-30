501a5877435dcaa72abbcdbc83e48bd13e478ae9
joaolfp <joaolucasfp2001@gmail.com>
2026-06-30 00:09:03 -0300

    fix: interrupted message

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,18 @@ use anyhow::Result;
 
 fn main() -> Result<()> {
 	let commands = commands::registry::all();
-	let selected = app::show_menu(&commands)?;
-	let input = app::get_user_input(&*commands[selected])?;
+
+	let selected = match app::show_menu(&commands) {
+		Ok(s) => s,
+		Err(e) if e.to_string().contains("interrupted") => return Ok(()),
+		Err(e) => return Err(e),
+	};
+
+	let input = match app::get_user_input(&*commands[selected]) {
+		Ok(i) => i,
+		Err(e) if e.to_string().contains("interrupted") => return Ok(()),
+		Err(e) => return Err(e),
+	};
+
 	commands[selected].execute(&input)
 }
