b463800ba87e69c941bf242b8758e4ab83b3ceaa
joaolfp <joaolucasfp2001@gmail.com>
2026-08-28 22:07:34 -0300

    feat: setup scripts as loop

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,21 @@ use anyhow::Result;
 fn main() -> Result<()> {
 	let commands = commands::registry::all();
 
-	let selected = match app::show_menu(&commands) {
-		Ok(s) => s,
-		Err(e) if e.to_string().contains("interrupted") => return Ok(()),
-		Err(e) => return Err(e),
-	};
+	loop {
+		let selected = match app::show_menu(&commands) {
+			Ok(s) => s,
+			Err(e) if e.to_string().contains("interrupted") => return Ok(()),
+			Err(e) => return Err(e),
+		};
 
-	let input = match app::get_user_input(&*commands[selected]) {
-		Ok(i) => i,
-		Err(e) if e.to_string().contains("interrupted") => return Ok(()),
-		Err(e) => return Err(e),
-	};
+		let input = match app::get_user_input(&*commands[selected]) {
+			Ok(i) => i,
+			Err(e) if e.to_string().contains("interrupted") => return Ok(()),
+			Err(e) => return Err(e),
+		};
 
-	commands[selected].execute(&input)
+		if let Err(e) = commands[selected].execute(&input) {
+			eprintln!("Error: {e:?}");
+		}
+	}
 }
